1f8e4d61a7e23ba8d94696bbc9cc6377e46ad1c9
unio <unioblog@gmail.com>
2019-11-05 10:07:54 +0900

    Updates

---
+++ precache-manifest.322ba0334e13a816e77494cfd6c2c415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b6795b0f47c20aeea0",
+    "url": "/portfolio/static/css/main.810de230.chunk.css"
+  },
+  {
+    "revision": "e9b6795b0f47c20aeea0",
+    "url": "/portfolio/static/js/main.e9b6795b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "cfab67a590d6c76b129d",
+    "url": "/portfolio/static/css/2.9eacbf8c.chunk.css"
+  },
+  {
+    "revision": "cfab67a590d6c76b129d",
+    "url": "/portfolio/static/js/2.cfab67a5.chunk.js"
+  },
+  {
+    "revision": "7d38e0d70ae6830af00cc5d93bef1e2a",
+    "url": "/portfolio/index.html"
+  }
+];
