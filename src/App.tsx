5ceb3b355e76525d1d47e45fbe3c8ddc21d29ad1
unio <unioblog@gmail.com>
2019-11-08 09:37:05 +0900

    buggyfillをuninstall, react-100-divをinstall

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
 import React, { FC } from 'react';
 import './App.css';
 
-//import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+// import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Div100vh from 'react-div-100vh';
 
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import Header from './components/Header';
 import TopNav from './containers/TopNav';
 import Editor from './containers/Editor';
 import Terminal from './containers/Terminal';
 import Footer from './components/Footer';
 
-import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
-
 const App: FC = () => {
-
   return (
-    <Grid container >
-      <Grid item xs={12}>
-        <Header />
-      </Grid>
-      <Grid container >
-        <Grid item xs={12} sm={2}>
-          <TopNav />
+    <Div100vh>
+      <Grid container>
+        <Grid item xs={12}>
+          <Header />
         </Grid>
-        <Grid item xs={12} sm={10}>
-          <Hidden xsDown>
-            <Editor />
-          </Hidden>
-          <Terminal />
+        <Grid container>
+          <Grid item xs={12} sm={2}>
+            <TopNav />
+          </Grid>
+          <Grid item xs={12} sm={10}>
+            <Hidden xsDown>
+              <Editor />
+            </Hidden>
+            <Terminal />
+          </Grid>
+          <Footer />
         </Grid>
-        <Footer />
       </Grid>
-    </Grid>
+    </Div100vh>
   );
 };
 
