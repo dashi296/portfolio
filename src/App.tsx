6b2c88ce29aaa32f3b6984f19a5de01478002e96
unio <unioblog@gmail.com>
2019-11-13 04:55:27 +0900

    remove React-Div-100vh

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React, { FC } from 'react';
 import './App.css';
 
 // import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Div100vh from 'react-div-100vh';
 
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
-import Header from './components/Header';
+import Header from './containers/Header';
 import TopNav from './containers/TopNav';
 import Editor from './containers/Editor';
 import Terminal from './containers/Terminal';
@@ -14,25 +13,23 @@ import Footer from './components/Footer';
 
 const App: FC = () => {
   return (
-    <Div100vh>
-      <Grid container>
-        <Grid item xs={12}>
-          <Header />
-        </Grid>
-        <Grid container>
-          <Grid item xs={12} sm={2}>
-            <TopNav />
-          </Grid>
-          <Grid item xs={12} sm={10}>
-            <Hidden xsDown>
-              <Editor />
-            </Hidden>
-            <Terminal />
-          </Grid>
-          <Footer />
-        </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
       </Grid>
-    </Div100vh>
+      <Grid container>
+        <Grid item xs={12} sm={2}>
+          <TopNav />
+        </Grid>
+        <Grid item xs={12} sm={10}>
+          <Hidden xsDown>
+            <Editor />
+          </Hidden>
+          <Terminal />
+        </Grid>
+        <Footer />
+      </Grid>
+    </Grid>
   );
 };
 
